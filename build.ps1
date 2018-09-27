8d04363848e41879c067c2e001bf439e83cf4161
Maxime Rouiller <angelzerosoft@gmail.com>
2018-09-27 15:44:22 -0400

    actually renamed the package

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 mkdir build -Force | Out-Null
 $targetNugetExe = ".\tools\nuget.exe"
 
-.$targetNugetExe pack MaximeRouiller.Dockerfile.Template.nuspec -OutputDirectory .\build
+.$targetNugetExe pack AzureAdvocates.Dockerfile.Template.nuspec -OutputDirectory .\build
